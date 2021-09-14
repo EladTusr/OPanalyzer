f84c87ec56b72fab3bd71c76339e2c2219a47987
EladTusr <90709304+EladTusr@users.noreply.github.com>
2021-09-14 18:18:17 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,22 +19,6 @@ namespace OPAnalyzer1
              Console.ReadLine();
 
 
-            /*     client.BaseAddress = new Uri("https://api.github.com/repos/highcharts/");
-                HttpResponseMessage response = client.GetAsync("highcharts/commits").Result;
-                response.EnsureSuccessStatusCode();
-                string result = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine("Result: " + result);
-                Console.WriteLine("Making API Call...");
-                 using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
-                 {
-                     // https://api.stackexchange.com/2.2/tags/highcharts/faq?site=stackoverflow
-                     client.BaseAddress = new Uri("https://api.stackexchange.com/2.2/tags/highcharts/");
-                     HttpResponseMessage response = client.GetAsync("faq?site=stackoverflow").Result;
-                     response.EnsureSuccessStatusCode();
-                     string result = response.Content.ReadAsStringAsync().Result;
-                     Console.WriteLine("Result: " + result);
-                 }
-                 Console.ReadLine();*/
 
 
         }
